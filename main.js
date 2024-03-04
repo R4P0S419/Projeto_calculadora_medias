1bb58824bf50fe703a563aff0aeb485057628d21
Raposa Foxyghostgm <r4p0s419@gmail.com>
2024-03-04 20:25:32 -0300

    primeiro commit do projeto

---
+++ main.js
@@ -0,0 +1,67 @@
+const form = document.getElementById("form-atividade");
+const imgAprovado = '<img src="./imagens/aprovado.png" alt="Emoji celebrando"';
+const imgReprovado = '<img src="./imagens/reprovado.png" alt="Emoji triste" />';
+const atividades = [];
+const notas = [];
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>';
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>';
+const NotaMinima = parseFloat(prompt('Digite a nota minima:'));
+
+let linhas = '';
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    adicionaLinha();
+    atualizaTabela();
+    atulizaMediaFinal();
+});
+
+function adicionaLinha() {
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert(`A atividade: ${inputNomeAtividade.value} ja foi inserida`);
+    } else {
+        atividades.push(inputNomeAtividade.value);
+    notas.push(parseFloat(inputNotaAtividade.value));
+
+    let linha = '<tr>';
+    linha += `<td> ${inputNomeAtividade.value}</td>`;
+    linha += `<td> ${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= NotaMinima ? imgAprovado : imgReprovado}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
+    
+    }
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+}
+
+function atualizaTabela(){
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
+
+function atulizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal();
+
+    document.getElementById('media-final-valor').innerHTML = mediaFinal;
+    document.getElementById('media-final-resultado').innerHTML = mediaFinal >= NotaMinima ? spanAprovado : spanReprovado;
+
+}
+
+function calculaMediaFinal (){
+    let somasDasNotas = 0;
+
+    for (let i = 0; i < notas.length; i++) {
+        somasDasNotas += notas[i];
+    }
+
+    return somasDasNotas / notas.length;
+}
+
